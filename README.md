647798e60703c4958403f3ca63b56113b147111f
JiadingGai <paul.paul.mit@gmail.com>
2017-05-27 10:04:06 -0400

    First commit of my favorite Bash scripts.

---
+++ README.md
@@ -0,0 +1,13 @@
+A collection of my favorite Bash scripts.
+
+## Getting Started
+
+1. [Hello.sh](scripts/Hello.sh): get a simple output
+
+## Programming
+
+1. [cpar.sh](scripts/cpar.sh): boilerplate for launching conveniently parallel jobs.
+
+## System Administration
+
+1. [Disk-Space.sh](scripts/Disk-Space.sh): check if the disk space crosses the limit
---
+++ scripts/cpar.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+display_usage() {
+  echo "\nUsage: sh $0 <your-msg>\n"
+}
+
+if [ "$#" -ne 1 ]
+then
+  display_usage
+  exit 1
+else
+  TheMsg=$1
+fi
+
+wait_jobs() {
+  JobBatchSize=$1 # Pass in $1 as the parallel batch size.
+
+  JobCounter=$((JobCounter + 1))
+  if ((JobCounter >= JobBatchSize)); then
+    for job in `jobs -p `; do
+      wait $job
+      ExitStatus=$?
+      echo "Job ID: $job ($ExitStatus)"
+      if [ $ExitStatus -ne 0 ]; then
+        let "FAIL=FAIL+1"
+      fi
+    done
+
+    if [ "$FAIL" == "0" ];
+    then
+      JobCounter=0
+    else
+      echo "Some job failed!"
+      exit 2
+    fi
+  fi
+}
+
+#----------------------------------------#
+#-------------- User Code ---------------#
+#----------------------------------------#
+
+a_job() {
+  sleep $(( (RANDOM % 15 ) + 1 ))
+}
+
+TotalNumberOfJobs=66
+MaxJobsInParallel=7
+JobCounter=0
+FAIL=0
+for ((i=0; i<$TotalNumberOfJobs; i++)); do
+  a_job &
+  echo "Launch Process ID: $! ($TheMsg)"
+  wait_jobs $MaxJobsInParallel # The number of jobs being launched in parallel
+done
+
+# Handle the residual jobs when TotalNumberOfJobs is not divible by MaxJobsInParallel
+wait_jobs 0
